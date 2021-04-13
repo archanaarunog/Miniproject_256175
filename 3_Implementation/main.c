ad6cbdf17fcd030cd2f77de5a610eb72c5fef76c
archanaarunog <68493803+archanaarunog@users.noreply.github.com>
2021-04-13 11:07:16 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -13,48 +13,64 @@ double* charact_eqn(int n, double *char_eqn)
 
 int function(double *ptr, int choice,int size)
 {
-    int m=(size/2)+2;
+    int m=(size/2)+1;
+    printf("%d",m);
     double det[size+1][m];
+    printf("\nSIZE=%d, %d",size+1,m);
     int i,j;
-    for(j=0;j<=m;j++)
-    {
-        for(i=0;i<2;i++)
-        {
-            if(j==m)
-            {
-                det[i][j]=0;
-                printf("%d det %d\n",i,j);
-            }
-            else
-            det[i][j]=*(ptr++); printf("%d  ptr %d  %lf\n",i,j,det[i][j]);
-        }
-    }
-    int lim=size;
-    for(i=2;i<=size+1;i++)
-    {
+
+     // fill up with all zeros
+     for(i=0;i<(size+1);i++)
         for(j=0;j<m;j++)
-        {
-            if(i>=lim)
-            {
-            det[i][j]=0;    
-            lim--;
-            }
-            else
-            {
-                 det[i][j]= ((det[i-1][0]*det[i-2][j+1])-(det[i-2][j-1]*det[i-1][j+1]))/det[i-1][0];
+            det[i][j]=0;
 
-            }
-        }
-    }
+     // fill up the first two rows
+        i=0;
+        for(j=0,i=0;(j<m)&&(i<=size);j++)
+	{
+            det[0][j]=*(ptr+(i++));
+	    if(i<=size)
+	    det[1][j]=*(ptr+(i++));
+	
+         }
 
-    for(i=0;i<size+1;i++)
-    {
-    for(j=0;j<m;j++)
-    {
-        printf("%lf\t",det[i][j]);
-    }
-    printf("\n");
-    }
+	// fill up subsequent rows
+       for(i=2;i<=size;i++)
+        for(j=0;j<(m-1);j++)
+
+	{
+             if (det[i-1][j]!=0)
+             det[i][j]= ((det[i-1][j]*det[i-2][j+1])-(det[i-2][j]*det[i-1][j+1]))/det[i-1][j];
+	
+         }
+
+     for(i=0;i<(size+1);i++)
+	{
+	printf("\n[");
+        for(j=0;j<m;j++)
+            printf("%lf\t",det[i][j]);
+	printf("]\n");
+	}
+      
+    //Check for sign changes in the first column
+    int prev_sign=0,changes=0,cur_sign=0;
+    if(det[0][0]>0)
+	prev_sign=1; //positive
+    else
+	prev_sign=0;  //negative
+     for(i=1;i<(size+1);i++)
+     {
+	if((det[i][0]>0&&prev_sign==0)||(det[i][0]<0)&&prev_sign==1)
+          {
+    changes++; 
+    if(det[i][0]>0)
+	prev_sign=1; //positive
+    else
+	prev_sign=0;  //negative          
+     }
+}
+   printf("\n Sign changes %d",changes);
+
     
 }
 
@@ -69,7 +85,7 @@ int main()
     scanf("%d",&n);
     double char_eqn[n];
     double* ptr=charact_eqn(n,char_eqn);
-    for(i=0;i<n;i++)
+    for(i=0;i<=n;i++)
     printf("%lf\n",*(ptr+i));
 
     printf("to display output press 1\nto display routh array press 2\n");
@@ -79,11 +95,3 @@ int main()
     
     return 0;
 }
-
-
-
-
-
-
-
-
